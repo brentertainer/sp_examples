d8bd4354eeabe8fd3a74a8f34bf271e41ae3b67c
brentertainer <42847267+brentertainer@users.noreply.github.com>
2021-10-08 11:57:06 -0500

    Inventory (#2)
    improved code documentation and more examples

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,35 +1,35 @@
-from functools import reduce
-from operator import mul
-
 import pyomo.environ as penv
 from mpisppy.scenario_tree import ScenarioNode
 
-import mpisppy.utils.sputils as sputils
-
 
 def instance_creator(name, num_days, cost, revenue, demand):
+    """
+    We formulate the inventory problem as a multi-stage stochastic program in which the
+    decision-maker faces an initial purchase decision followed by a series of sales decisions.
+    Initially, he or she must choose how much product to order to satisfy uncertain demand over a
+    finite time horizon. Each day, he or she must determine how much inventory to sell. Daily sales
+    may not exceed daily demand. The per-unit purchase cost is deterministic and fixed. The
+    per-unit sales revenue is also deterministic but differs from day to day. The demand also
+    varies from day to day and is stochastic with a (possibly different) discrete distribution each
+    day. No other costs or revenues are considered.
+    """
 
-    def obj_by_stage(inst, day):
+    def profit_by_day(inst, day):
         if day == 0:
+            # In day 0, purchase costs are incurred and there is no revenue.
             return -inst.cost * inst.inventory[day]
         else:
+            # In each subsequent day, sales revenues are realized and there is no cost.
             return inst.revenue[day] * inst.sales[day]
 
-    def obj_overall(inst):
-        return sum(inst.obj_by_stage[n] for n in inst.days)
-
-    def con_y0_is_zero(inst):
-        return inst.sales[0] == 0
+    def profit_overall(inst):
+        return sum(inst.profit_by_day[n] for n in inst.days)
 
     def con_product_conservation(inst, day):
+        # The inventory at the end of today is equal to the inventory at the end of yesterday less
+        # today's sales volume.
         if day != 0:
-            return inst.inventory[day] == inst.inventory[day-1] - inst.sales[day]
-        else:
-            return penv.Constraint.Skip
-
-    def con_sales_le_inventory(inst, day):
-        if day != 0:
-            return inst.sales[day] <= inst.inventory[day-1]
+            return inst.inventory[day] == inst.inventory[day - 1] - inst.sales[day]
         else:
             return penv.Constraint.Skip
 
@@ -39,62 +39,65 @@ def instance_creator(name, num_days, cost, revenue, demand):
     inst.cost = penv.Param(default=0, initialize=cost)
     inst.revenue = penv.Param(inst.days, default=0, initialize=revenue)
     inst.demand = penv.Param(inst.days, default=0, initialize=demand)
-    inst.inventory = penv.Var(inst.days, domain=penv.NonNegativeReals) # at end of day
+    # Inventory is measured after sales at end of each day.
+    inst.inventory = penv.Var(inst.days, bounds=(0, None))
+    # Daily sales may not exceed daily demand.
     inst.sales = penv.Var(inst.days, bounds=lambda inst, n: (0, inst.demand[n]))
-    inst.obj_by_stage = penv.Expression(inst.days, rule=obj_by_stage)
-    inst.obj_overall = penv.Objective(sense=penv.maximize, rule=obj_overall)
-    inst.con_y0_is_zero = penv.Constraint(rule=con_y0_is_zero)
+    # The following expression set breaks down the objective by stage. We do this for convenience
+    # of later telling which part of the objective results from each stage.
+    inst.profit_by_day = penv.Expression(inst.days, rule=profit_by_day)
+    inst.profit_overall = penv.Objective(sense=penv.maximize, rule=profit_overall)
     inst.con_product_conservation = penv.Constraint(inst.days, rule=con_product_conservation)
-    inst.con_sales_le_inventory = penv.Constraint(inst.days, rule=con_sales_le_inventory)
     return inst
 
 
-def node_creator(instance, path, probability):
-    nodes = []
-    nodes.append(ScenarioNode('ROOT',
-                              1.0,
-                              1,
-                              instance.obj_by_stage[0],
-                              None,
-                              [instance.inventory[0],
-                               instance.sales[0]],
-                              instance))
-    for day, val in enumerate(path[:-1], start=1):
-        this_name = '_'.join(['ROOT', *[str(level) for level in path[:day]]])
-        if day == 1:
-            parent_name = 'ROOT'
-        else:
-            parent_name = '_'.join(['ROOT', *[str(level) for level in path[:day-1]]])
-        nodes.append(ScenarioNode(this_name,
-                                  probability[path[day-1]],
-                                  day+1,
-                                  instance.obj_by_stage[day],
-                                  None,
-                                  [instance.inventory[day],
-                                   instance.sales[day]],
-                                  instance,
-                                  parent_name=parent_name))
-    return nodes
-
-
-def scenario_creator(name,
-                     paths=None,
+def scenario_creator(scen,
+                     nodes=None,
+                     parent=None,
+                     node_prob=None,
+                     scen_prob=None,
                      num_days=None,
                      cost=None,
                      revenue=None,
-                     demand=None,
-                     probability=None):
+                     demand=None):
+    """
+    In mpi-sppy, the basic idea is to one determinsitic model per leaf node in the scenario tree.
+    The mpi-sppy model object (ExtensiveForm, PH, LShapedMethod, etc.) then sews the determinstic
+    models together with non-anticipativity constraints.
 
-    path = tuple(map(int, paths[name].split('_')[1:]))
-    instance = instance_creator(name,
+    The `scenario_creator` function is a dynamic tool for generating the various scenarios. To
+    create a scenario, first build a Pyomo model that incorporates all scenario stages. Then attach
+    some probability-related data to the scenario for mpi-sppy to later use. First, attach the list
+    of non-leaf nodes from the scenario tree that characterize the scenario. The list should start
+    from and include the root node (which must be named 'ROOT'). The conditional probability of
+    each node must be supplied at the time of construction. The list should be attached to the
+    Pyomo model through the `_mpisppy_node_list` attribute. Second, attach the joint probability of
+    the scenario to through the `_mpisppy_probability` attribute.
+
+    Note that each scenario is given its own instances of scenario tree nodes. Here's an example.
+    On pencil and paper, every scenario shares the root node of the scenario tree; however, in
+    mpi-sppy each scenario generates its own root node object (i.e., a distinct root node with a
+    unique memory address but with properties identical to all other root node objects).
+    """
+    # Create an instance of the scenario using the `instance_creator` function.
+    instance = instance_creator(scen,
                                 num_days=num_days,
                                 cost=cost,
-                                revenue=revenue,
-                                demand={day: demand[val] for day, val in enumerate(path, start=1)})
-
-    # The following had me stumped for quite a while.
-    # When we create the nodes, we have to specify conditional probabilities.
-    # But when we create the scenarios, we have to specify *joint* probabilities.
-    instance._mpisppy_node_list = node_creator(instance, path, probability)
-    instance._mpisppy_probability = reduce(mul, [probability[level] for level in path])
+                                revenue=revenue[scen],
+                                demand=demand[scen])
+    # Attach the list of non-tree nodes from the scenario tree that characterize the scenario.
+    instance._mpisppy_node_list = [
+        ScenarioNode(node,
+                     node_prob[node],
+                     day+1,
+                     instance.profit_by_day[day],
+                     None,
+                     [instance.inventory[day],
+                      instance.sales[day]],
+                     instance,
+                     parent_name=parent[node])
+        for day, node in enumerate(nodes[scen])
+    ]
+    # Attach the joint probability of the scenario.
+    instance._mpisppy_probability = scen_prob[scen]
     return instance
--- inventory/inventory_ef.py
+++ inventory/inventory_ef.py
@@ -1,51 +1,32 @@
+from functools import reduce
 from itertools import product
+from operator import mul
 
 import pyomo.environ as penv
 from mpisppy.opt.ef import ExtensiveForm
 
 import inventory
+import balanced_3stage as data
+#import balanced_6stage as data
+#import unbalanced_4stage as data
 
-
-# instance parameters
-"""
-num_days = 2
-levels = [0, 1] # 0 = low demand, 1 = high demand
-cost = 5
-revenue = {1: 7, 2: 5}
-probability = {0: 0.25, 1: 0.75}
-demand = {0: 5, 1: 10}
-"""
-
-# instance parameters
-num_days = 5
-levels = [0, 1, 2]
-cost = 5
-revenue = {1: 8, 2: 7, 3: 6, 4: 5, 5: 4}
-probability = {0: 0.25, 1: 0.50, 2: 0.25}
-demand = {0:  8, 1: 10, 2: 12}
-
-
-# ExtensiveForm arguments
-scenarios = [f'Scen{i}' for i in range(1, len(levels) ** num_days + 1)]
-scenario_creator = inventory.scenario_creator
-paths = dict()
-scen_iter = iter(scenarios)
-for prod in product(levels, repeat=num_days):
-    paths[next(scen_iter)] = '_'.join(['ROOT', *[str(level) for level in prod]])
-scenario_creator_kwargs = {
-    'paths': paths,
-    'num_days': num_days,
-    'cost': cost,
-    'revenue': revenue,
-    'demand': demand,
-    'probability': probability
-}
-all_nodenames = ['ROOT']
-for n in range(1, num_days + 1):
-    for prod in product(levels, repeat=n):
-        all_nodenames.append('_'.join(['ROOT', *[str(level) for level in prod]]))
 
 options = {'solver': 'gurobi', 'mipgap': 0.0}
+all_nodenames = data.all_nodenames
+scenarios = data.scenarios
+scenario_creator = inventory.scenario_creator
+scenario_creator_kwargs = {
+    # scenario tree data
+    'nodes': data.nodes,
+    'parent': data.parent,
+    'node_prob': data.node_prob,
+    'scen_prob': data.scen_prob,
+    # scenario instance data
+    'num_days': data.num_days,
+    'cost': data.cost,
+    'revenue': data.revenue,
+    'demand': data.demand
+}
 
 ef = ExtensiveForm(options,
                    scenarios,
@@ -53,8 +34,25 @@ ef = ExtensiveForm(options,
                    scenario_creator_kwargs=scenario_creator_kwargs,
                    all_nodenames=all_nodenames)
 
+result = ef.solve_extensive_form()
+print('Objective:', penv.value(ef.ef.EF_Obj))
+print('Purchase:', penv.value(ef.ef.Scen1.inventory[0]))
 
-ef.solve_extensive_form()
-print('Objective: ', penv.value(ef.ef.EF_Obj))
-print('First Stage Decision: ', penv.value(ef.ef.Scen1.inventory[0]))
-#ef.report_var_values_at_rank0()
+
+nodes = set()
+edges = set()
+for scen, path in data.nodes.items():
+    for a, b in zip(path, path[1:] + [scen]):
+        nodes.add(a)
+        nodes.add(b)
+        edges.add((a, b))
+
+import networkx as nx
+import matplotlib.pyplot as plt
+graph = nx.DiGraph()
+graph.add_nodes_from(sorted(nodes))
+graph.add_edges_from(sorted(edges))
+
+pos = nx.drawing.nx_pydot.graphviz_layout(graph, "twopi")
+nx.draw(graph, pos, with_labels=True)
+plt.show()
